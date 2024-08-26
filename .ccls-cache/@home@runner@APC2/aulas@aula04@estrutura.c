f8af3e13806f427dba3de850b4ec52947b201836
lovis11 <162124343+lovis11@users.noreply.github.com>
2024-08-26 14:00:56 +0000

    aula 04 1.1

--- .ccls-cache/@home@runner@APC2/aulas@aula04@estrutura.c
+++ .ccls-cache/@home@runner@APC2/aulas@aula04@estrutura.c
@@ -13,20 +13,36 @@ int main(void) {
   printf("Nome: %s\n", pessoa1.nome);
   printf("Idade: %d\n", pessoa1.idade);
   printf("Gênero: %c\n", pessoa1.genero);
-
+  
   struct Pessoa pessoa2;
 
-  printf("Entre com o nome da pessoa: \n ");
+  printf("Entre com o nome da pessoa: ");
   int deu_certo = scanf("%[^\n]s", pessoa2.nome);
-  printf("Entre com a idade da pessoa: \n ");
+  printf("Entre com a idade da pessoa: ");
     deu_certo = scanf("%d", &pessoa2.idade);
   getchar();
-  printf("Entre com M ou F para o gênero da pessoa: \n ");
+  printf("Entre com M ou F para o gênero da pessoa: ");
     deu_certo = scanf("%c", &pessoa2.genero);
+  if (pessoa2.genero == 'M') {
+    pessoa2.genero = 'M';
+    printf("Nome: %s\n", pessoa2.nome);
+    printf("Idade: %d\n", pessoa2.idade);
+    printf("Gênero: %c\n", pessoa2.genero);
+  } else if (pessoa2.genero == 'F') {
+    pessoa2.genero = 'F';
+    printf("Nome: %s\n", pessoa2.nome);
+    printf("Idade: %d\n", pessoa2.idade);
+    printf("Gênero: %c\n", pessoa2.genero);
+  } else {
+    printf("Caractere inválido!\n");
+  } 
 
-  printf("Nome: %s\n", pessoa2.nome);
-  printf("Idade: %d\n", pessoa2.idade);
-  printf("Gênero: %c\n", pessoa2.genero);
+  struct Pessoa *p_pessoa;
+  p_pessoa = &pessoa1;
+  // (*ponteiro_pessoa).genero;
+  printf("Nome: %s\n", (*p_pessoa).nome);
+  printf("Idade: %d\n", (*p_pessoa).idade);
+  printf("Gênero: %c\n", (*p_pessoa).genero);
   
   
   return 0;
